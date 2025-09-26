5da9ad480c658c84b8448304bbcf52211e116095
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-27 00:00:52 +0700

    Create index.js

---
+++ src/core/math/Color/index.js
@@ -0,0 +1,12 @@
+export { Color } from './Color.js';
+export { BasicColors } from './BasicGlazeColor.js';
+export { ExtendedColors } from './ExtendedGlazeColors.js';
+export { 
+  ColorFactory, 
+  ColorShortcut, 
+  lerpColors, 
+  mixColors, 
+  addColors, 
+  multiplyColors, 
+  invertColor 
+} from './FullColorFactory.js';
