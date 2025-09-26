343d57adcd93b9d652582b6f8fa4de7d65cad9a3
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-27 00:36:13 +0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+export * from './core/index.js';
+export * from './geometries/index.js';
+export * from './lights/index.js';
+export * from './math/index.js';
+export * from './loaders/index.js';
+export * from './controls/index.js';
+export * from './utils/index.js';
