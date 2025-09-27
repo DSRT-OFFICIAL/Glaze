3cffda6a3f4898966922a4d2cc4eef0e10371905
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-27 23:23:24 +0700

    Create index.js

---
+++ src/gradients/index.js
@@ -0,0 +1,6 @@
+// ==================== gradients/index.js ====================
+
+// Ekspor semua modul gradasi
+export * from "./linear.js";
+export * from "./radial.js";
+export * from "./conic.js";
