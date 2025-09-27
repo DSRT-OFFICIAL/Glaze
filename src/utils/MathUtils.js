7a076217dc81992c31976564895260652bc7b8d6
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-27 07:33:12 +0700

    Update MathUtils.js

--- src/utils/MathUtils.js
+++ src/utils/MathUtils.js
@@ -1,18 +1,9 @@
-// Math helper untuk Glaze
 export const MathUtils = {
-    DEG2RAD: Math.PI / 180,
-    RAD2DEG: 180 / Math.PI,
-
-    clamp: (value, min, max) => Math.max(min, Math.min(max, value)),
-
+    clamp: (v, min, max) => Math.max(min, Math.min(max, v)),
     lerp: (a, b, t) => a + (b - a) * t,
-
-    map: (value, inMin, inMax, outMin, outMax) => 
-        outMin + (value - inMin) * (outMax - outMin) / (inMax - inMin),
-
-    randomFloat: (min=0, max=1) => Math.random() * (max - min) + min,
-    randomInt: (min, max) => Math.floor(Math.random() * (max - min + 1)) + min,
-
-    degToRad: (deg) => deg * MathUtils.DEG2RAD,
-    radToDeg: (rad) => rad * MathUtils.RAD2DEG
+    degToRad: (d) => d * Math.PI / 180,
+    radToDeg: (r) => r * 180 / Math.PI,
+    randomRange: (min, max) => Math.random() * (max - min) + min,
+    rangeMap: (value, inMin, inMax, outMin, outMax) =>
+        ((value - inMin) / (inMax - inMin)) * (outMax - outMin) + outMin
 };
