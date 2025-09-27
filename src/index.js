80b07f59e972f37de2bdc182a5901bb7d609adb1
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-27 07:30:51 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
+// -----------------------
+// Core
+// -----------------------
 export * from './core/index.js';
+
+// -----------------------
+// Geometries
+// -----------------------
 export * from './geometries/index.js';
+
+// -----------------------
+// Lights
+// -----------------------
 export * from './lights/index.js';
-export * from './math/index.js';
-export * from './loaders/index.js';
+
+// -----------------------
+// Controls
+// -----------------------
 export * from './controls/index.js';
+
+// -----------------------
+// Loaders
+// -----------------------
+export * from './loaders/index.js';
+
+// -----------------------
+// Utils
+// -----------------------
 export * from './utils/index.js';
