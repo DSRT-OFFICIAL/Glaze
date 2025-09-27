70c25ff76af836fdc97522a5bed418f9d3815350
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-28 02:01:05 +0700

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,27 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import terser from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript";
 
 export default [
   // ESM
   {
-    input: "src/index.js",
+    input: "src/index.ts", // bisa index.js juga, kalau TS tetap generate d.ts
     output: {
       file: "dist/glaze.esm.js",
       format: "esm",
-      sourcemap: true
+      sourcemap: true,
     },
-    plugins: [resolve(), commonjs(), terser()]
+    plugins: [resolve(), commonjs(), typescript({ declaration: true, outDir: "dist" }), terser()],
   },
   // CJS
   {
-    input: "src/index.js",
+    input: "src/index.ts",
     output: {
       file: "dist/glaze.cjs.js",
       format: "cjs",
-      sourcemap: true
+      sourcemap: true,
     },
-    plugins: [resolve(), commonjs(), terser()]
-  }
+    plugins: [resolve(), commonjs(), typescript({ declaration: false }), terser()],
+  },
 ];
