917b3b8db6f8432491013e8c3b8ecd6bfc6bf3c5
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-27 23:29:12 +0700

    Create index.js

---
+++ src/palettes/index.js
@@ -0,0 +1,7 @@
+// ==================== palettes/index.js ====================
+
+// Ekspor semua palet warna
+export * from "./GlazeBasicColors.js";
+export * from "./GlazeExtendedColors.js";
+export * from "./GlazeVariantsColors.js";
+export * from "./userPalettes.js"; // jika ada palet khusus pengguna
