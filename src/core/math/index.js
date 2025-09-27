4bb5509399a8b09525d04cc549fd43f7aa1af354
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-27 07:29:48 +0700

    Update index.js

--- src/core/math/index.js
+++ src/core/math/index.js
@@ -1,16 +1,13 @@
-export * from './Color/index.js';
+// Export semua kelas math & color
 export * from './Vector2.js';
 export * from './Vector3.js';
-export * from './Vector4.js';
-export * from './Matrix3.js';
 export * from './Matrix4.js';
 export * from './Quaternion.js';
 export * from './Euler.js';
-export * from './Plane.js';
-export * from './Ray.js';
-export * from './Sphere.js';
-export * from './Box3.js';
-export * from './Frustum.js';
-export * from './MathUtils.js';
-export * from './Interpolation.js';
-export * from './Polar.js';
+
+// Color
+export * from './Color/Color.js';
+export * from './Color/BasicGlazeColor.js';
+export * from './Color/ExtendedGlazeColors.js';
+export * from './Color/FullColorFactory.js';
+export * from './Color/DebugColorUtils.js';
