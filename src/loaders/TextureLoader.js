d84bf7b676ecac08410ed5a285fb234806afb221
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-27 07:28:56 +0700

    Create TextureLoader.js

---
+++ src/loaders/TextureLoader.js
@@ -0,0 +1,13 @@
+export class TextureLoader {
+    constructor() {}
+
+    load(url) {
+        return new Promise((resolve, reject) => {
+            const img = new Image();
+            img.crossOrigin = "anonymous";
+            img.onload = () => resolve(img);
+            img.onerror = (err) => reject(err);
+            img.src = url;
+        });
+    }
+}
