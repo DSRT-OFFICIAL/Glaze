2f0f091c1d1b243a80581f4b7cf3cbc786756e0c
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-27 23:53:43 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,40 +1,31 @@
-// ==================== index.js ====================
+// ==================== Glaze Master Index ====================
 
-// ---------- Import per kategori ----------
-import * as Conversion from "./Colors/ColorsConversion.js";
-import * as Manipulation from "./Colors/ColorsManipulation.js";
-import * as Accessibility from "./Colors/ColorsAccessibility.js";
-import * as Theme from "./Colors/ColorsTheme.js";
-import * as Palette from "./Colors/PaletteUtils.js";
-import * as Random from "./Colors/ColorsRandom.js";
-import * as Alias from "./Colors/ColorsAlias.js";
+// Core
+export * from "./core/index.js";
 
-import * as Basic from "./Colors/GlazeBasicColors.js";
-import * as Extended from "./Colors/GlazeExtendedColors.js";
-import * as Variants from "./Colors/GlazeVariantsColors.js";
+// Conversion
+export * from "./conversion/index.js";
 
-// ---------- Flat Export ----------
-export * from "./Colors/ColorsConversion.js";
-export * from "./Colors/ColorsManipulation.js";
-export * from "./Colors/ColorsAccessibility.js";
-export * from "./Colors/ColorsTheme.js";
-export * from "./Colors/PaletteUtils.js";
-export * from "./Colors/ColorsRandom.js";
-export * from "./Colors/ColorsAlias.js";
-export * from "./Colors/GlazeBasicColors.js";
-export * from "./Colors/GlazeExtendedColors.js";
-export * from "./Colors/GlazeVariantsColors.js";
+// Manipulation
+export * from "./manipulation/index.js";
 
-// ---------- Namespace Export ----------
-export {
-  Conversion,
-  Manipulation,
-  Accessibility,
-  Theme,
-  Palette,
-  Random,
-  Alias,
-  Basic,
-  Extended,
-  Variants,
-};
+// Accessibility
+export * from "./accessibility/index.js";
+
+// Gradients
+export * from "./gradients/index.js";
+
+// Palettes
+export * from "./palettes/index.js";
+
+// Random
+export * from "./random/index.js";
+
+// Effects
+export * from "./effects/index.js";
+
+// Analysis (Pro features)
+export * from "./analysis/index.js";
+
+// IO (Import/Export)
+export * from "./io/index.js";
