a2b82391aed054ae67942045250386a69201f60f
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-27 23:35:54 +0700

    Create index.js

---
+++ src/random/index.js
@@ -0,0 +1,10 @@
+// ==================== index.js ====================
+
+// Warna acak
+export * from "./randomColor.js";
+
+// Palet acak
+export * from "./randomPalette.js";
+
+// Gradient acak
+export * from "./randomGradient.js";
