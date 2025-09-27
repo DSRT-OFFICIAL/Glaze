8458d6b930369134109706cc052fd15e2f060bd0
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-27 23:05:50 +0700

    Create index.js

---
+++ src/manipulation/index.js
@@ -0,0 +1,10 @@
+// ==================== index.js ====================
+
+// Core color manipulation functions
+export * from "./lighten.js";
+export * from "./darken.js";
+export * from "./saturate.js";
+export * from "./desaturate.js";
+export * from "./blend.js";
+export * from "./mix.js";
+export * from "./invert.js";
