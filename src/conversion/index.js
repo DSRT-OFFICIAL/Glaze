c9a9549b4fffdfd35bd2d0fa6b8bb01b08af26df
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-27 23:03:27 +0700

    Update index.js

--- src/conversion/index.js
+++ src/conversion/index.js
@@ -1,1 +1,9 @@
+// ==================== index.js ====================
 
+// Core conversions
+export * from "./rgb.js";
+export * from "./hsl.js";
+export * from "./cmyk.js";
+export * from "./lab.js";
+export * from "./xyz.js";
+export * from "./hsv.js";
