349bae451776125c7964b6095c167cd29250f0c1
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-27 23:51:05 +0700

    Create index.js

---
+++ src/io/index.js
@@ -0,0 +1,7 @@
+// ==================== index.js ====================
+
+// Import semua modul IO
+export * from "./hex.js";
+export * from "./css.js";
+export * from "./json.js";
+export * from "./svg.js";
