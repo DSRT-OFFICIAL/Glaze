84fda829861072977dd13841346612b9550935a0
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-28 20:38:30 +0700

    Create GlazePalettes.js

---
+++ src/palettes/GlazePalettes.js
@@ -0,0 +1,123 @@
+import { GlazeExpertColors } from "./GlazeExpertColors.js";
+
+export const GlazePalettes = {
+  // ---------------- Light Palettes ----------------
+  light: [
+    GlazeExpertColors.white,
+    GlazeExpertColors.gray100,
+    GlazeExpertColors.gray200,
+    GlazeExpertColors.gray300,
+    GlazeExpertColors.gray400,
+    GlazeExpertColors.slate100,
+    GlazeExpertColors.slate200,
+    GlazeExpertColors.slate300,
+    GlazeExpertColors.clouds,
+    GlazeExpertColors.mintCream,
+  ],
+
+  // ---------------- Dark Palettes ----------------
+  dark: [
+    GlazeExpertColors.black,
+    GlazeExpertColors.gray700,
+    GlazeExpertColors.gray800,
+    GlazeExpertColors.gray900,
+    GlazeExpertColors.slate700,
+    GlazeExpertColors.slate800,
+    GlazeExpertColors.slate900,
+    GlazeExpertColors.midnightBlue,
+    GlazeExpertColors.wetAsphalt,
+    GlazeExpertColors.asbestos,
+  ],
+
+  // ---------------- Pastel Palettes ----------------
+  pastel: [
+    GlazeExpertColors.pastelPink,
+    GlazeExpertColors.pastelOrange,
+    GlazeExpertColors.pastelYellow,
+    GlazeExpertColors.pastelGreen,
+    GlazeExpertColors.pastelBlue,
+    GlazeExpertColors.pastelPurple,
+    GlazeExpertColors.pastelMint,
+    GlazeExpertColors.pastelPeach,
+    GlazeExpertColors.pastelLavender,
+    GlazeExpertColors.pastelRose,
+    GlazeExpertColors.pastelSky,
+    GlazeExpertColors.pastelLime,
+    GlazeExpertColors.pastelCoral,
+    GlazeExpertColors.pastelMagenta,
+  ],
+
+  // ---------------- Vibrant Palettes ----------------
+  vibrant: [
+    GlazeExpertColors.vibrantRed,
+    GlazeExpertColors.vibrantOrange,
+    GlazeExpertColors.vibrantYellow,
+    GlazeExpertColors.vibrantGreen,
+    GlazeExpertColors.vibrantBlue,
+    GlazeExpertColors.vibrantPurple,
+    GlazeExpertColors.vibrantPink,
+    GlazeExpertColors.vibrantCyan,
+    GlazeExpertColors.vibrantLime,
+    GlazeExpertColors.vibrantMagenta,
+    GlazeExpertColors.vibrantTeal,
+    GlazeExpertColors.vibrantIndigo,
+    GlazeExpertColors.vibrantAmber,
+    GlazeExpertColors.vibrantDeepOrange,
+  ],
+
+  // ---------------- Corporate Palettes ----------------
+  corporate: [
+    GlazeExpertColors.facebookBlue,
+    GlazeExpertColors.twitterBlue,
+    GlazeExpertColors.linkedinBlue,
+    GlazeExpertColors.slackPurple,
+    GlazeExpertColors.youtubeRed,
+    GlazeExpertColors.pinterestRed,
+    GlazeExpertColors.whatsappGreen,
+    GlazeExpertColors.tiktokBlack,
+    GlazeExpertColors.githubBlack,
+    GlazeExpertColors.instagramGradientStart,
+    GlazeExpertColors.instagramGradientEnd,
+    GlazeExpertColors.nephritis,
+    GlazeExpertColors.belizeHole,
+    GlazeExpertColors.amethyst,
+    GlazeExpertColors.sunFlower,
+  ],
+
+  // ---------------- Mixed UI Palettes ----------------
+  uiNeutral: [
+    GlazeExpertColors.white,
+    GlazeExpertColors.gray200,
+    GlazeExpertColors.gray400,
+    GlazeExpertColors.gray600,
+    GlazeExpertColors.gray800,
+    GlazeExpertColors.black,
+  ],
+
+  uiAccent: [
+    GlazeExpertColors.peterRiver,
+    GlazeExpertColors.turquoise,
+    GlazeExpertColors.amethyst,
+    GlazeExpertColors.sunFlower,
+    GlazeExpertColors.carrot,
+    GlazeExpertColors.alizarin,
+  ],
+
+  uiPastelAccent: [
+    GlazeExpertColors.pastelPink,
+    GlazeExpertColors.pastelBlue,
+    GlazeExpertColors.pastelGreen,
+    GlazeExpertColors.pastelPurple,
+    GlazeExpertColors.pastelPeach,
+    GlazeExpertColors.pastelRose,
+  ],
+
+  uiVibrantAccent: [
+    GlazeExpertColors.vibrantRed,
+    GlazeExpertColors.vibrantOrange,
+    GlazeExpertColors.vibrantYellow,
+    GlazeExpertColors.vibrantGreen,
+    GlazeExpertColors.vibrantBlue,
+    GlazeExpertColors.vibrantPurple,
+  ],
+};
