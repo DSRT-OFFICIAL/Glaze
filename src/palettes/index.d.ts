0de9fb04fe36e8deec58ec1cb6e829a77e8bca77
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-28 20:00:33 +0700

    Update index.d.ts

--- src/palettes/index.d.ts
+++ src/palettes/index.d.ts
@@ -5,16 +5,16 @@ import { GlazeExtendedColors } from "./GlazeExtendedColors";
 import { GlazeVariantsColors } from "./GlazeVariantsColors";
 import { GlazeNamedColors } from "./GlazeNamedColors";
 import { GlazeGradients, generateGradient } from "./GlazeGradients";
-import { GlazeNamedGradients } from "./GlazeNamedColors";
 import * as GlazeUtils from "./GlazeUtils";
-import { GlazeAll } from "./GlazeAll";
+import * as GlazeAll from "./GlazeAll";
 
 export { GlazeBasicColors };
 export { GlazeExtendedColors };
 export { GlazeVariantsColors };
 export { GlazeNamedColors };
 export { GlazeGradients };
-export { GlazeNamedGradients }; 
 export { generateGradient };
 export { GlazeUtils };
-export { GlazeAll }; 
+
+// All-in-one export
+export { GlazeAll };
