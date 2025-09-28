5fbf3f66f1fb3ae6a748a288d2a54a07efa83aa8
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-28 19:42:39 +0700

    Update index.js

--- src/palettes/index.js
+++ src/palettes/index.js
@@ -1,16 +1,15 @@
 // ==================== index.js ====================
-// Entry point utama Glaze Library
 
 // Warna dasar, extended, variant
 export { GlazeBasicColors } from "./GlazeBasicColors.js";
 export { GlazeExtendedColors } from "./GlazeExtendedColors.js";
 export { GlazeVariantsColors } from "./GlazeVariantsColors.js";
 
-// Warna lengkap dengan semua format
+// Warna lengkap dengan semua format (hex, rgb, rgba, hsv, hsl, cmyk)
 export { GlazeNamedColors } from "./GlazeNamedColors.js";
 
-// Gradients preset
-export { GlazeGradients } from "./GlazeGradients.js";
+// Gradients preset dan helper
+export { GlazeGradients, generateGradient } from "./GlazeGradients.js";
 
 // Semua helper / util
 export * as GlazeUtils from "./GlazeUtils.js";
