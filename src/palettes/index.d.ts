3a690ba4cf6632f6787a3a824fe59655f6c5b715
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-28 19:40:44 +0700

    Create index.d.ts

---
+++ src/palettes/index.d.ts
@@ -0,0 +1,15 @@
+// ==================== index.d.ts ====================
+
+import { GlazeBasicColors } from "./GlazeBasicColors";
+import { GlazeExtendedColors } from "./GlazeExtendedColors";
+import { GlazeVariantsColors } from "./GlazeVariantsColors";
+import { GlazeNamedColors } from "./GlazeNamedColors";
+import { GlazeGradients } from "./GlazeGradients";
+import * as GlazeUtils from "./GlazeUtils";
+
+export { GlazeBasicColors };
+export { GlazeExtendedColors };
+export { GlazeVariantsColors };
+export { GlazeNamedColors };
+export { GlazeGradients };
+export { GlazeUtils };
